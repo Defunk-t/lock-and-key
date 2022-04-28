420379176e91d995a5403c7ca885e7f867917bce
Defunkt <86127892+Defunk-t@users.noreply.github.com>
2022-04-28 01:49:03 +0100

    Redo File and FileCached docs

--- electron/data/file.js
+++ electron/data/file.js
@@ -1,41 +1,37 @@
 const {readFile, writeFile, rm} = require('fs/promises');
 
+/**
+ * @typedef DataFile
+ * @property {function: Promise<string|null>} get Get the current data value.
+ * @property {function: Promise<void>} del Remove the file from the disk.
+ * @property {function(payload:string): Promise<void>} set Overwrite the file with the payload.
+ * @property {function: Promise<boolean>} isSet Check if data/file exists.
+ */
+
+/**
+ * Returns an interface for managing a data file.
+ * @param {string} path
+ * @returns DataFile
+ * @constructor
+ */
 const File = path => {
 
-	/**
-	 * Read the file contents. Returns a promise that resolves with the files contents in a string if it exists,
-	 * otherwise resolves `NULL`.
-	 * @return Promise<?string>
-	 */
 	const get = () => {
 		console.log(`Reading from ${path}`);
 		return readFile(path, 'utf8')
 			.catch(() => null);
 	};
 
-	/**
-	 * Deletes the file.
-	 * @return Promise<void>
-	 */
 	const del = () => rm(path);
 
 	return {
 
-		/**
-		 * Overwrite the file with the given data payload.
-		 * @param {string} data
-		 * @return Promise<void>
-		 */
 		set: data => {
 			console.log(`Writing to ${path}`);
 			const write = () => writeFile(path, data, {mode: 0o400});
 			return del().then(write, write);
 		},
 
-		/**
-		 * Check if this file or data exists.
-		 * @return Promise<boolean>
-		 */
 		isSet: () =>
 			get().then(value => !!value),
 
@@ -44,6 +40,13 @@ const File = path => {
 	};
 };
 
+/**
+ * Returns an interface for managing a data file.
+ * File's current value is kept in memory.
+ * @param {string} path
+ * @returns DataFile
+ * @constructor
+ */
 const FileCached = path => {
 
 	const file = File(path);
@@ -52,33 +55,21 @@ const FileCached = path => {
 
 	/**
 	 * Current value of file - kept in memory to limit filesystem IOs.
-	 * @type ?string
+	 * @type string|null
 	 */
 	let currentValue;
 
-	/**
-	 * Read the file contents or cached value. Returns a promise that resolves with the files contents in a string
-	 * if it exists, otherwise resolves `NULL`. Value is kept in memory after first call to reduce filesystem IOs.
-	 * @return Promise<?string>
-	 */
-	file.get = () => !!currentValue
+	const newGet = () => !!currentValue
 		? new Promise(resolve => resolve(currentValue))
 		: get().then(data => currentValue = data);
 
-	/**
-	 * Overwrite the file with the given data payload.
-	 * @param {string} data
-	 * @return Promise<void>
-	 */
+	file.get = newGet;
+
 	file.set = data =>
 		set(currentValue = data);
 
-	/**
-	 * Check if this file or data exists.
-	 * @return Promise<boolean>
-	 */
 	file.isSet = () =>
-		get().then(value => !!value);
+		newGet().then(value => !!value);
 
 	return file;
 };
